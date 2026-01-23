5f1665bb6990ec2704f66436e488d23c22944f3d
youthdreamer <95559199+Youthdreamer@users.noreply.github.com>
2026-01-24 01:06:14 +0800

    fix:添加中文符号作为软换行标志，小幅优化中英混写时的换行问题

--- config/core/autocmd.nix
+++ config/core/autocmd.nix
@@ -43,14 +43,15 @@
     }
 
     {
-      desc = "部分文档文件开启自动软换行";
+      desc = "部分文档文件开启自动软换行并添加中文符号作为换行标志";
       event = "FileType";
-      pattern = ["markdown" "md" "text"];
+      pattern = ["markdown" "text"];
       callback = {
         __raw = ''
           function()
             vim.opt_local.wrap = true
             vim.opt_local.linebreak = true
+            vim.opt_local.breakat:append("，。！？；：")
           end
         '';
       };
